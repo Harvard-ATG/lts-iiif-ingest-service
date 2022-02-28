b33a83884c5d828bf3a3403414a7438a15f7f1bd
Cole Crawford <cole_crawford@fas.harvard.edu>
2022-02-28 16:17:18 -0500

    Update generate_manifest.py

--- generate_manifest.py
+++ generate_manifest.py
@@ -7,11 +7,11 @@ def createManifest(
     base_url: str,
     labels: list, 
     canvases: list,
-    behaviors: list =["paged"],
+    behaviors: list = ["paged"],
     manifestMetadata: list = None,
     rights: str = None,
     required_statement: list = None
-    ) -> iiifpapi3.Manifest():
+) -> iiifpapi3.Manifest():
     """ Creates and validates a IIIF manifest """
     iiifpapi3.BASE_URL = base_url
     manifest = iiifpapi3.Manifest()
@@ -27,7 +27,7 @@ def createManifest(
                 label=m["label"],
                 value=m["value"],
                 language_l=m["label_lang"],
-                value_l=m["value_lang"]
+                language_v=m["value_lang"]
             )
     
     if(rights):
@@ -39,23 +39,23 @@ def createManifest(
                 label=m["label"],
                 value=m["value"],
                 language_l=m["label_lang"],
-                value_l=m["value_lang"]
+                language_v=m["value_lang"]
             )
     
     for idx,d in enumerate(canvases):
         idx+=1 
         canvas = manifest.add_canvas_to_items()
         canvas.set_id(extendbase_url="canvas/p%s"%idx) # in this case we use the base url
-        canvas.set_height(d[2])
-        canvas.set_width(d[1])
-        canvas.add_label("en",d[0])
-        if canvas["metadata"]:
-            for m in canvas["metadata"]:
+        canvas.set_height(d["height"])
+        canvas.set_width(d["width"])
+        canvas.add_label("en",d["label"])
+        if "metadata" in d:
+            for m in d["metadata"]:
                 canvas.add_metadata(
                     label=m["label"],
                     value=m["value"],
                     language_l=m["label_lang"],
-                    value_l=m["value_lang"]
+                    language_v=m["value_lang"]
                 )
 
         annopage = canvas.add_annotationpage_to_items()
@@ -63,13 +63,13 @@ def createManifest(
         annotation = annopage.add_annotation_to_items(target=canvas.id)
         annotation.set_id(extendbase_url="annotation/p%s-image"%str(idx).zfill(4))
         annotation.set_motivation("painting")
-        annotation.body.set_id("".join(d[3:]))
+        annotation.body.set_id(f"{d['id']}{d['service']}")
         annotation.body.set_type("Image")
         annotation.body.set_format("image/jpeg")
-        annotation.body.set_width(d[1])
-        annotation.body.set_height(d[2])
+        annotation.body.set_width(d["width"])
+        annotation.body.set_height(d["height"])
         s = annotation.body.add_service()
-        s.set_id(d[3])
+        s.set_id(d["id"])
         s.set_type("ImageService3")
         s.set_profile("level1")
     
@@ -98,30 +98,61 @@ def test():
         base_url="https://iiif.io/api/cookbook/recipe/0009-book-1/",
         labels=[{"lang": "en", "label":"Simple Manifest - Book"}],
         #        label       width height id                                                                            service
-        canvases = (("Blank page",3204,4613,"https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f18","/full/max/0/default.jpg"),
-                ("Frontispiece",3186,4612,"https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f19","/full/max/0/default.jpg"),
-                ("Title page",3204,4613,"https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f20","/full/max/0/default.jpg"),
-                ("Blank page",3174,4578,"https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f21","/full/max/0/default.jpg"),
-                ("Bookplate",3198,4632,"https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f22","/full/max/0/default.jpg"),),
-        behaviors=["paged"]
+        canvases = [
+            { "label": "Blank page", "width": 3204, "height": 4613, "id": "https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f18", "service": "/full/max/0/default.jpg" },
+            { "label": "Frontispiece", "width": 3186, "height": 4612, "id": "https://iiif.io/api/image/3.0/example/reference/59d09e6773341f28ea166e9f3c1e674f-gallica_ark_12148_bpt6k1526005v_f19", "service":"/full/max/0/default.jpg",
+                "metadata": [
+                    {
+                        "label": "Reference",
+                        "value": "ID124",
+                        "label_lang": "en",
+                        "value_lang": "en"
+                    }
+                ]
+             }
+        ],
+        behaviors=["paged"],
+        rights = "http://creativecommons.org/licenses/by-sa/3.0/",
+        required_statement=[
+            {
+                "label": "Attribution",
+                "value": "Cole Crawford",
+                "label_lang": "en",
+                "value_lang": "en"
+            }
+        ],
+        manifestMetadata=[
+            {
+                "label": "Creator",
+                "value": "Unknown",
+                "label_lang": "en",
+                "value_lang": "en"
+            },
+            {
+                "label": "Date",
+                "value": "19th Century",
+                "label_lang": "en",
+                "value_lang": "en"
+            }
+        ]
     )
-    print(manifest)
     manifest.json_save("test-manifest.json")
+    return manifest
 
-class Canvas:
-    """Represents a IIIF canvas"""
-    def __init__(self, label: str, id: str, width: int = None, height: int = None, metadata: list = None, service: str = "/full/max/0/default.jpg"):
-        self.label = label
-        self.id = id
-        self.service = service
-        if(not width or not height):
-            # Get and set these
-            print("no width and/or height passed")
-        else:
-            self.width = width
-            self.height = height
-        if metadata:
-            self.metadata = metadata
+# class Canvas:
+#     """Represents a IIIF canvas"""
+#     def __init__(self, label: str, id: str, width: int = None, height: int = None, metadata: list = None, service: str = "/full/max/0/default.jpg"):
+#         self.label = label
+#         self.id = id
+#         self.service = service
+#         if(not width or not height):
+#             # Get and set these
+#             print("no width and/or height passed")
+#         else:
+#             self.width = width
+#             self.height = height
+#         if metadata:
+#             self.metadata = metadata
 
 
 if __name__ == '__main__':
