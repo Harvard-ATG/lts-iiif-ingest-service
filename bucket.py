c6f39e5a20c841e9c399f559faaa84bc0b815c54
Cole Crawford <cole_crawford@fas.harvard.edu>
2022-03-01 19:43:08 -0500

    Better image uploading

--- bucket.py
+++ bucket.py
@@ -1,10 +1,51 @@
 import os
 import logging
 import boto3
+from botocore.exceptions import ClientError
 from settings import ROOT_DIR
 import sys, argparse
+from PIL import Image
 
 logger = logging.getLogger(__name__)
+
+def upload_image_get_metadata(image_path, bucket_name, s3_path="", session=None):
+    if not session:
+        session = boto3._get_default_session()
+    s3 = session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+
+    file_dir, file_name = os.path.split(image_path)
+    # make sure s3_path ends in a slash
+
+    # extend to handle in-memory later, once we have file upload on an application
+    # https://thecodinginterface.com/blog/aws-s3-python-boto3/
+    # bytes_data = 
+    # obj = s3.Object(bucket_name, f"{s3_path}{file_name}")
+    # obj.put(Body=bytes_data)
+
+    # get the image metadata
+    img = Image.open(image_path)
+    width, height = img.size
+    format = img.get_format_mimetype()
+
+    key = f"{s3_path}{file_name}" if s3_path else file_name
+
+    # try to upload it
+    try:
+        bucket.upload_file(
+            Filename=image_path,
+            Key=key
+        )
+        return {
+            # "response": response, # upload_file doesn't return a response?
+            "filename": key,
+            "height": height,
+            "width": width,
+            "format": format
+        }
+    except ClientError as e:
+        logging.error(e)
+        return False
 
 def upload_directory(path, bucket_name, session=None):
     if not session:
@@ -20,13 +61,19 @@ def upload_directory(path, bucket_name, session=None):
 
 
 if __name__ == "__main__":
-    print(f"Arguments count: {len(sys.argv)}")
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
-    
     parser = argparse.ArgumentParser()
     parser.add_argument("--dir", "-d", help="set directory")
     parser.add_argument("--bucket", "-b", help="set bucket")
+    parser.add_argument("--file", "-f", help="set single file for upload")
+    parser.add_argument("--s3path", help="Optional S3 path")
     args = parser.parse_args()
 
-    upload_directory(args.dir, args.bucket)
+    if(not args.bucket):
+        print("Please set a bucket which you can use in the current AWS session")
+
+    if(args.dir and args.bucket):
+        upload_directory(args.dir, args.bucket)
+    
+    if(args.file and args.bucket):
+        response = upload_image_get_metadata(args.file, args.bucket, s3_path=args.s3path)
+        print(response)
