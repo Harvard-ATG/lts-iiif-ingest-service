8ec91eb1124a406529d15c49a4fc47dbbd3931c4
Vesna Tan <tanvezi@gmail.com>
2022-05-03 14:49:56 -0400

    add pytest raises value error test

--- src/IIIFingest/tests/test_asset.py
+++ src/IIIFingest/tests/test_asset.py
@@ -2,8 +2,10 @@ import pytest
 import os.path
 from PIL import Image
 
-from ..asset import get_image_size
+from ..asset import get_image_size, Asset
 
+def broken_asset_id():
+    return Asset(asset_id="test-ing")
 
 def test_get_image_size():
     abs_path = os.path.dirname(os.path.abspath(__file__))
@@ -13,5 +15,7 @@ def test_get_image_size():
     assert w == 3600
     assert h == 564
 
-
+def test_asset_raises_value_error():
+    with pytest.raises(ValueError):
+        broken_asset_id()
 
