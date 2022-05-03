8fa65f99d19bfa98893f4fecca047b90e453dae3
Vesna Tan <tanvezi@gmail.com>
2022-05-03 14:56:29 -0400

    add test created from file

--- src/IIIFingest/tests/test_asset.py
+++ src/IIIFingest/tests/test_asset.py
@@ -4,12 +4,13 @@ from PIL import Image
 
 from ..asset import get_image_size, Asset
 
+abs_path = os.path.dirname(os.path.abspath(__file__))
+image_path = os.path.join(abs_path, "images", "27.586.1-cm-2016-02-09.tif")
+
 def broken_asset_id():
     return Asset(asset_id="test-ing")
 
 def test_get_image_size():
-    abs_path = os.path.dirname(os.path.abspath(__file__))
-    image_path = os.path.join(abs_path, "images", "27.586.1-cm-2016-02-09.tif")
     w, h = get_image_size(image_path)
     # assert (w, h) == (3600, 1)
     assert w == 3600
@@ -18,4 +19,11 @@ def test_get_image_size():
 def test_asset_raises_value_error():
     with pytest.raises(ValueError):
         broken_asset_id()
+
+def test_asset_created_from_file():
+    asset = Asset.from_file(image_path)
+    assert asset.format == "image/tiff"
+    assert asset.extension == ".tiff"
+    assert asset.width == 3600
+    assert asset.height == 564
 
